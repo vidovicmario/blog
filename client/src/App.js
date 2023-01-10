339c2ce1c1567265ef419bd348a44370359d8592
vidovicmario <vidovicmario97@gmail.com>
2023-01-10 13:41:42 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+//import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
